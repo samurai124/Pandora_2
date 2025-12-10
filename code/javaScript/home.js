af32f5abf82542c05b8381ca563e4dd6771c65be
Hamza <hamzazaidi253@gmail.com>
2025-12-10 12:05:07 +0100

    basic

--- code/javaScript/home.js
+++ code/javaScript/home.js
@@ -1,45 +1,82 @@
-let apiKey = "232d0db3036cadd3faf87eca64b548ea";
-const cities = ["Paris", "Tokyo", "Madrid"];
-const city = "Paris";
+function serchFunction() {
+  if (document.querySelector(".search_box").style.display == "inline") {
+    document.querySelector(".search_box").style.display = "none";
+    document.querySelector(".results_container").style.display = "none";
+  } else {
+    document.querySelector(".search_box").style.display = "inline";
+    document.querySelector(".results_container").style.display = "flex";
+  }
+}
 
 
-if ("geolocation" in navigator) {
+
+const input = document.getElementById("search_box");
+input.addEventListener("input", (e) => {
+  console.log(e.target.value);
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const wrapper = document.getElementById('cardsWrapper');
+    const scrollLeftBtn = document.getElementById('scrollLeftBtn');
+    const scrollRightBtn = document.getElementById('scrollRightBtn');
+    
+    const scrollDistance = 195;
+    const scrollCards = (direction) => {
+        const newScrollLeft = wrapper.scrollLeft + (direction * scrollDistance);
+        
+        wrapper.scrollTo({
+            left: newScrollLeft,
+            behavior: 'smooth'
+        });
+    };
+    scrollLeftBtn.addEventListener('click', () => {
+        scrollCards(-1); 
+    });
+    scrollRightBtn.addEventListener('click', () => {
+        scrollCards(1); 
+    });
+});
+
+let apiKey = "6802086343806005e8c57c443f7d07ca";
+let latitude;
+let longitude;
+function getLocation() {
   navigator.geolocation.getCurrentPosition(
-    async (position) => {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-
-      console.log("Latitude:", lat);
-      console.log("Longitude:", lon);
-
-      try {
-        const res = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
-        );
-
-        if (!res.ok) {
-          throw new Error("Failed to fetch weather data");
-        }
-
-        const data = await res.json();
-
-        console.log("City:", data.name);
-        console.log("Temp:", data.main.temp);
-        console.log("Weather:", data.weather[0].description);
-
-      } catch (err) {
-        console.log("API Error:", err.message);
-      }
+    (position) => {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      console.log("Latitude:", latitude);
+      console.log("Longitude:", longitude);
+      getWeather(latitude, longitude);
     },
     (error) => {
-      console.log("Location Error:", error.message);
+      console.error("Error getting location:", error.message);
     },
     {
       enableHighAccuracy: true,
       timeout: 10000,
-      maximumAge: 0
+      maximumAge: 0,
     }
   );
-} else {
-  console.log("Geolocation is not supported by this browser.");
 }
+
+async function getWeather(lat, lon) {
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch weather data");
+    }
+    const data = await res.json();
+    console.log("City:", data.name);
+    console.log("Temperature:", data.main.temp + "°C");
+    console.log("Weather:", data.weather[0].description);
+  } catch (err) {
+    console.log("API Error:", err.message);
+  }
+}
+
+getLocation();
